6c038efe70323447ffb44c78509088685c99d4ad
Stepan <Stepashkabasketball@mail.ru>
2025-02-19 14:44:20 +0700

    try one

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((initialCards, deleteCard) => {
+  const placesList = document.querySelector(".places__list");
+  const cardsTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardsTemplate
+    .querySelector(".places__item")
+    .cloneNode("true");
+
+  cardElement.querySelector(".card__image").src = initialCards.link;
+  cardElement.querySelector(".card__image").alt = initialCards.name;
+  cardElement.querySelector(".card__title").textContent = initialCards.name;
+
+  placesList.append(cardElement);
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(deleteButton);
+  });
+
+  deleteCard = (deleteButton) => {
+    const toDelete = deleteButton.closest(".places__item");
+    toDelete.remove();
+  };
+});
+
